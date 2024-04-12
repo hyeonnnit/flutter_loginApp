a97c2d69f647d6e529cf772f72c52b1cbf98baae
lhyeonnit <lhyeonnit@gmail.com>
2024-04-12 16:08:11 +0900

    login_page / ListView

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:login_app/components/logo.dart';
+import 'package:login_app/size.dart';
 
 class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            SizedBox(height: xlarfe_gap),
+            Logo("Login"),
+          ],
+        ),
       ),
     );
   }
