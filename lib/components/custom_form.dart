a64c0c9e0022c48894ad831a6c9c23bceb4ee512
lhyeonnit <lhyeonnit@gmail.com>
2024-04-12 16:24:28 +0900

    CustomForm

---
+++ lib/components/custom_form.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:login_app/components/custom_text_form_field.dart';
+import 'package:login_app/size.dart';
+
+class CustomForm extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          CustomTextFormField("Email"),
+          SizedBox(height: medium_gap),
+          CustomTextFormField("Password"),
+          SizedBox(height: large_gap),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:login_app/pages/home_page.dart';
+import 'package:login_app/pages/login_page.dart';
 
-import 'pages/home_page.dart';
-import 'pages/login_page.dart';
+
 
 
 
